ed6bd5b2f6220a9dd2d607dd73e4bb899f9a4ca9
mlodyk <kacperzagrodzki048@gmail.com>
2025-02-14 19:27:47 +0100

    foldery

--- app.js
+++ app.js
@@ -11,19 +11,12 @@ window.addEventListener('load', function () {
   // console.log(buttons[6].offsetWidth)
 
   for (let element of buttons) {
-    // element.setAttribute("style",`background-image: url('./img${i}.jpg');`)
+    element.setAttribute("style",`background-image: url('./zdjecia/img${i}.jpg');`)
     element.style.setProperty('--title-letter-spacing', calculateLetterSpacing(element));
     i++;
 
-
-    
     element.addEventListener("click", async function (e) {
       console.log(this.offsetHeight);
-      // let pierwsy=this.offsetWidth
-      // await sleep(1000)
-      // console.log(this.offsetWidth);
-      // let drugi=this.offsetWidth
-      // console.log(pierwsy-drugi)
 
     });
 
