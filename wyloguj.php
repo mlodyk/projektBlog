f62eea42b2bc23514b0a6dac4885ea5a7d13a77b
harypota1 <70764897+harypota1@users.noreply.github.com>
2025-02-16 20:43:18 +0100

    Update wyloguj.php

--- wyloguj.php
+++ wyloguj.php
@@ -1,11 +1,8 @@
 <?php
 session_start();
-
-// Usuwamy wszystkie dane sesji
 session_unset();
 session_destroy();
 
-// Przekierowujemy użytkownika na stronę logowania
 header("Location: login.php");
 exit();
 ?>
